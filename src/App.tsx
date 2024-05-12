30793052d0c8eff6c04bec3415ebfc85edc47845
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2024-05-12 15:20:01 +0500

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import "./App.css";
-
+import { Button, ButtonGroup } from "@chakra-ui/react";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
 }
 
 export default App;
